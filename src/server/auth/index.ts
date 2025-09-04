4f28f11680e6018e77f6cbfdf237179d7b02344f
Frank17008 <405267330@qq.com>
2025-09-04 17:26:44 +0800

    feat(auth): 调整auth认证目录结构

---
+++ src/server/auth/index.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import {
+  db,
+  users,
+  accounts,
+  sessions,
+  verificationTokens,
+} from "@/server/db/schema";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  providers: [GitHub],
+  trustHost: true,
+});
