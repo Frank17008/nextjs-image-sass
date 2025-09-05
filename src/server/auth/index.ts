fb65ec347e8ffb47a664e453c07d44939b880139
Frank17008 <405267330@qq.com>
2025-09-05 14:57:24 +0800

    refactor(auth): 重构导入语句以提高代码清晰度
    - 将 db 导入语句从解构赋值改为直接导入
    - 调整了导入的顺序和格式，使代码更加简洁明了

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -2,12 +2,12 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import {
-  db,
   users,
   accounts,
   sessions,
   verificationTokens,
 } from "@/server/db/schema";
+import { db } from "@/server/db/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db, {
