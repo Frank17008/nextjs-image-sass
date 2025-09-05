7c8c889d4137ae3f24f9a24c08ad43286e840aa8
Frank17008 <405267330@qq.com>
2025-09-05 10:43:09 +0800

    feat(api): 枚举验证
    - 在用户信息查询中加入 provider 字段的查询和验证
    - 新增 authProviderSelectSchema 用于验证 provider 数据
    - 修改 GET 请求处理函数，返回用户信息时包含 provider 数据

--- src/app/api/test/[...id]/route.ts
+++ src/app/api/test/[...id]/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
-import { userSelectSchema } from "@/server/db/validate-schema";
+import {
+  userSelectSchema,
+  authProviderSelectSchema,
+} from "@/server/db/validate-schema";
 import { db } from "@/server/db/db";
-import { users } from "@/server/db/schema";
+import { users, accounts } from "@/server/db/schema";
 import { eq } from "drizzle-orm";
 
 export async function GET(req: NextRequest) {
@@ -23,5 +26,12 @@ export async function GET(req: NextRequest) {
   if (!result.success) {
     return NextResponse.json({ message: "username is not found" });
   }
-  return NextResponse.json(result.data);
+  // 查询provider
+  const rows2 = await db
+    .select()
+    .from(accounts)
+    .where(eq(accounts.userId, result.data.id))
+    .limit(1);
+  const result2 = authProviderSelectSchema.safeParse(rows2[0].provider);
+  return NextResponse.json({ ...result.data, provider: result2.data });
 }
--- src/server/db/validate-schema.ts
+++ src/server/db/validate-schema.ts
@@ -1,6 +1,11 @@
 import { users } from "./schema";
 import { createSelectSchema, createInsertSchema } from "drizzle-zod";
 
+import { pgEnum } from "drizzle-orm/pg-core";
+
+const authProvider = pgEnum("authProvider", ["github", "gitlab"]);
+export const authProviderSelectSchema = createSelectSchema(authProvider);
+
 export const userSelectSchema = createSelectSchema(users);
 
 export const userInsertSchema = createInsertSchema(users);
