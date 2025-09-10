0d2b5ff5ba82be88744fa45b8aa13730b02277b6
Frank17008 <405267330@qq.com>
2025-09-10 09:34:40 +0800

    refactor(app): 重构首页组件并移除未使用的数据库代码
    - 移除了未使用的 TRPC 和数据库相关代码
    - 更新了表单布局和样式，增加了密码输入框
    - 修改了登录按钮的文本内容
    - 删除了未

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import SignIn, { SessionProvider } from "./sign-in";
-import { useTRPC } from "@/utils/trpc";
-import { useQuery } from "@tanstack/react-query";
+import SignIn from "./sign-in";
 
 export default function Home() {
-  const trpc = useTRPC();
-  const query = useQuery(trpc.hello.queryOptions());
-  console.log(query.data?.message);
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <h1 className="mb-5 text-2xl font-bold">Create App</h1>
-      <form action="" className="w-full max-w-md flex flex-col gap-y-2.5">
-        <Input name="username" placeholder="App Name"></Input>
-        <Textarea
-          name="description"
-          placeholder="App Description"
-          className="resize-none"
-        ></Textarea>
+      <h1 className="mb-5 text-2xl font-bold">用户登录</h1>
+      <form action="" className="w-full max-w-md flex flex-col gap-y-5">
+        <Input name="username" placeholder="请输入用户名"></Input>
+        <Input name="password" type="password" placeholder="请输入密码"></Input>
         <Button type="submit" className="cursor-pointer">
-          Submit
+          登录
         </Button>
       </form>
-      <SessionProvider>
-        <SignIn />
-      </SessionProvider>
+      <SignIn />
     </div>
   );
 }
