8288c756965c6c69b6a1d04d06ce397694ee1aed
Frank17008 <405267330@qq.com>
2025-09-11 16:19:52 +0800

    ``` chore(gitignore, package.json): 更新.gitignore和package.json
    在.gitignore中添加了对.vscode目录的忽略规则，以避免将VSCode配置文件提交到仓库。
    在package.json中添加了@radix-ui/react-dropdown-menu依赖，版本为^2.1.16，以便支持新的下拉菜单组件。
    ```

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,21 +1,52 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { auth } from "@/server/auth";
-async function DashboardLayout({
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
+  const { data: session } = useSession();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
+  const routeToGithub = () => {
+    window.open("https://github.com/Frank17008/nextjs-image-sass");
+  };
   return (
     <div className="flex h-full w-full flex-col">
       <div className="flex  h-16 w-full items-center justify-end border-b px-4">
-        <div className="flex items-center gap-4">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src={session?.user?.image as string} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <div>{session?.user?.name}</div>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center gap-4">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={session?.user?.image as string} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div>{session?.user?.name}</div>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={routeToGithub}>GitHub</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={async () => {
+                await signOut({ callbackUrl: "/" });
+              }}
+            >
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       {children}
     </div>
